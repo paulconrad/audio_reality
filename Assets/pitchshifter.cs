617bac4e132edd480a82d5b885b38494a4bde4a4
Colin Wright <cdotwright@gmail.com>
2019-01-26 16:04:56 -0800

    added unity project, moved audio to /assets

---
+++ Assets/pitchshifter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class pitchshifter : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public float targetDistance;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the AudioSource from the GameObject
+        audioSource = GetComponent<AudioSource>();
+
+        //Initialize the pitch
+        audioSource.pitch = 2.0f;
+        if(playerCamera == null){
+            //defaults to main camera. not sure why we would change this.. 
+            playerCamera = Camera.main.gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerCamera != null)
+        {
+            targetDistance = Vector3.Distance(gameObject.transform.position, playerCamera.transform.position);
+            if (targetDistance < 2.0f) {
+                audioSource.pitch = targetDistance;
+            }
+            Debug.Log(targetDistance);
+        }
+        
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Global Game Jam 2019 AR project
 
 Just testing commits! <3 paul teehee
+
+The root folder is the unity project folder. 
+the scene we are working with is called "audio reality" in the editor, this is built off the example unity-ARkit project.
+if you want to add stuff, please do it in the assets folder so unity can find it easily.
+
