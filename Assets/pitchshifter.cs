22813a761eb8a01f581012de5c0eafed459d64a7
Colin Wright <cdotwright@gmail.com>
2019-01-26 20:04:11 -0800

    updated scene with controls/ completion state for audio target. moved audio to assets.

--- Assets/pitchshifter.cs
+++ Assets/pitchshifter.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class pitchshifter : MonoBehaviour
 {
+    //ui parameters
     public GameObject playerCamera;
-    public float targetDistance;
-    AudioSource audioSource;
+    public float pitchStartValue;
+    public float pitchEndValue;
+    public AudioClip successAudioClip;
 
+
+    //other
+    AudioSource audioSource;
+    float playerDistance;
+    float pitchBorder;
+    float pitchTarget;
+    bool success;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,24 +24,50 @@ public class pitchshifter : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
 
         //Initialize the pitch
-        audioSource.pitch = 2.0f;
+        audioSource.pitch = pitchStartValue;
         if(playerCamera == null){
             //defaults to main camera. not sure why we would change this.. 
             playerCamera = Camera.main.gameObject;
         }
+        pitchBorder = transform.GetChild(0).localScale.x / 2.0f;
+        pitchTarget = transform.GetChild(1).localScale.x / 2.0f;
+        success = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (playerCamera != null)
+        if ((playerCamera != null) && !success)
         {
-            targetDistance = Vector3.Distance(gameObject.transform.position, playerCamera.transform.position);
-            if (targetDistance < 2.0f) {
-                audioSource.pitch = targetDistance;
+            playerDistance = Vector3.Distance(gameObject.transform.position, playerCamera.transform.position);
+            if (playerDistance < pitchBorder)
+            {
+                if (playerDistance > pitchTarget)
+                {
+                    audioSource.pitch = ((playerDistance - pitchTarget) / (pitchBorder - pitchTarget)) * (pitchStartValue - pitchEndValue) + pitchEndValue;
+                }
+                else
+                {
+                    audioSource.pitch = pitchEndValue;
+                }
             }
-            Debug.Log(targetDistance);
-        }
+
+            //Debug.Log(targetDistance);
+        } 
+        
+    }
+    void OnTriggerEnter(Collider other){
+        other.gameObject.GetComponent<AudioSource>().PlayOneShot(successAudioClip);
+        success = true;
+        audioSource.pitch = 1.0f;
+        
+    }
+    void OnTriggerStay(Collider other){
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
         
     }
 }
